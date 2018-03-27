eb004f99f8a5a8764b65a2e9c05ec2b71e72df91
Titov Daniil <dt122333@gmail.com>
2018-03-27 17:23:34 +0300

    Changed 2nd lab

--- lab2/task1.py
+++ lab2/task1.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import json
 
 
 def get_data_from_json(path="data.json"):
@@ -6,88 +7,33 @@ def get_data_from_json(path="data.json"):
     :param path: string type path to json file 
     :return: pandas DataFrame object
     """
-    df = pd.read_json(path)
-    df1 = pd.DataFrame(data=df["table"]["rows"], columns=df["table"]["columnNames"])
-
-    df1["time_created"] = pd.to_datetime(df1["time_created"])
-    df1["time"] = pd.to_datetime(df1["time"])
-    df1["time_modified"] = pd.to_datetime(df1["time_modified"])
-
-    return df1
-
-
-def remove_not_zero_rows(df, columns=None, trace=True):
-    """
-    This method removes all rows that contains value more 0 for 'columns' parameters
-    :param df: pandas DataFrame
-    :param columns: Array of strings 
-    :param trace: If True there is message about removed rows, else no message. 
-    :return: Processed DataFrame
-    """
-
-    if columns is None:
-        columns = [
-            "current_speed_qc",
-            "current_direction_qc",
-            "current_u_qc",
-            "current_v_qc",
-            "temperature_qc",
-            "conductivity_qc",
-            "salinity_qc",
-            "sigma_t_qc"
-        ]
-
-    source_size = len(df)
-    for column in columns:
-        df.drop(df[df[column] > 0].index, inplace=True)
-
-    if trace:
-        print("Removed", source_size - len(df), "rows")
-
+    data = pd.read_json(path)
+    df = pd.DataFrame(data=data["table"]["rows"], columns=data["table"]["columnNames"])
     return df
 
 
-def get_min_max_parameters(df, parameters=None):
-    """
-    Method return list of dict of min, max, min_index, max_index for all 'parameters'
-    :param df: Pandas DataFrame
-    :param parameters: Array of strings
-    :return: Value like [parameter1: {ind_min: ..., ind_max: ...}, parameter2: {ind_min: ..., ind_max: ...}, ...]
-    """
+def get_parameter_values(parameter, data):
 
-    if parameters is None:
-        parameters = [
-            "temperature",
-            "salinity",
-            "conductivity"
-        ]
-
-    result = list()
-
-    for parameter in parameters:
-
-        ind_max = df[parameter].idxmax()
-        # max_val = df[parameter][ind_max]
-
-        ind_min = df[parameter].idxmin()
-        # min_val = df[parameter][ind_min]
-
-        result.append({parameter: {
-            "ind_min": ind_min,
-            "ind_max": ind_max,
-            # "min": min_val,
-            # "max": max_val
-        }})
+    clear_data = data.drop(data[data[parameter + "_qc"] != 0].index)
+    result = {"start_date": clear_data["time"].min(),
+              "end_date": clear_data["time"].max(),
+              "num_records": len(clear_data),
+              "min_" + parameter: clear_data[parameter].min(),
+              "min_time": clear_data["time"].min(),
+              "max_" + parameter: clear_data[parameter].min(),
+              "max_time": clear_data["time"].max(),
+              "avg_" + parameter: clear_data[parameter].mean()}
 
     return result
 
-if __name__ == '__main__':
-    df = get_data_from_json("data.json")
-    df = remove_not_zero_rows(df)
-    labels = ["current_speed", "current_direction"]
-    parameters = get_min_max_parameters(df, labels)
 
-    for i in parameters:
-        row_indexes = list(list(i.values())[0].values())
-        print("For parameter", list(i.keys())[0], ":")
-        print(df.iloc[row_indexes, :], end="\n" + "=" * 60 + "\n\n\n")
+def process_data(parameters, data):
+    result = {parameter: get_parameter_values(parameter, data) for parameter in parameters}
+    return json.dumps(result, indent=4)
+
+
+if __name__ == '__main__':
+    parameters = ["current_speed", "temperature", "salinity"]
+    data = get_data_from_json("data.json")
+    processed_data = process_data(parameters, data)
+    print(processed_data)
